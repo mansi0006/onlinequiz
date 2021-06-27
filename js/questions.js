19f56b32ec0efdafb033dac04c4c695c5fe89256
mansi0006 <86471826+mansi0006@users.noreply.github.com>
2021-06-27 10:34:55 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,95 @@
+//creating an array and passing number, questions, answers and options
+let questions = [
+    {
+        numb: 1,
+        question: "what is recurrence for worst case of Quick sort and what is the time complexity in worst?",
+        answer: "Recurrence is T(n) = T(n-1)+O(n) and time complexity is O(n^2)",
+        options: [
+            "Recurrence is T(n) = T(n-2)+O(n) and time complexity is O(n^2)",
+            "Recurrence is T(n) = T(n-1)+O(n) and time complexity is O(n^2)",
+            "Recurrence is T(n) = T(n)+O(n) and time complexity is O(n^3)",
+            "Recurrence is T(n) = T(n+1)+O(n+1) and time complexity is O(n^2)",
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the full form of CSS?",
+    answer: "Cascading style sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading style sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which process can be affected by other processes executing in the system?",
+    answer: "cooperating process",
+    options: [
+        "cooperating process",
+        "child process",
+         "parent process",
+         "init process",
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  // we will continue likewise.
+  {
+    numb: 6,
+    question: "What is the time complexity of pop() operation when the stack is implemented using an array?",
+    answer: "O(1)",
+    options: [
+        "O(1)",
+        "O(n)",
+        "O(logn)",
+        "O(nlogn)",
+    ]  
+  },
+  {
+    numb: 7,
+    question: " Minimum number of queues to implement stack is __________?",
+    answer: "1",
+    options: [
+        "3",
+        "4",
+        "1",
+        "2",
+    ]
+  },
+{
+    numb: 8,
+    question: " In a circular queue, how do you increment the rear end of the queue?",
+    answer: "(rear+1) % CAPACITY",
+    options: [
+        "rear++",
+        "(rear+1) % CAPACITY",
+        "(rear % CAPACITY)+1",
+        "rear–",
+    ] 
+},
+   
+
+
+];
